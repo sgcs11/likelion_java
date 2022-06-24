6ec8c7976c1b3a7a86dec72c63007c17d3164c4c
sgcs11 <joachy@naver.com>
2022-06-24 15:47:08 +0900

    작업 1

---
+++ src/main/java/org/example/likelion/Main.java
@@ -0,0 +1,14 @@
+package org.example.likelion;
+
+// 문제 : 자동차 설계도를 만들어주세요.
+class Car {
+    ;
+}
+
+class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        System.out.println("Hello world!");
+    }
+}
+
